b5de1c0e55346d20b4e04a4b1a1bc94d11336d20
dario molina <darvak8@gmail.com>
2014-11-24 00:42:34 -0300

    ejemplo de simulacion de movimiento de 2 cuerpos geometricos

--- simulacionEjemplo.py
+++ simulacionEjemplo.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+# Ejemplo de simulacion de movimiento de dos cuerpos geometricos aplicando vpython
+
 import visual
 # flecha=visual.arrow(pos=visual.vector(0,0,0), axis=(-3,-5,-1), color=visual.color.white)
-# caja=visual.box(pos=visual.vector(4,2,3), size=(0.5,0.4,0.6), color=visual.color.red)
+suelo=visual.box(pos=visual.vector(0,-1,0), size=(10,0.1,10), color=visual.color.red)
 # bola=visual.sphere(pos=visual.vector(-3,0,0), radius=0.5, color=visual.color.green)
 
 class Ball(visual.sphere):
 	def __init__(self):
  		visual.sphere.__init__(self,radius=0)
  		self.velocity=(0,0,0)
+ 		self.acceleration=(0,0,0)
  		self.masa=0
 
  	def setPos(self, pos):
@@ -26,6 +31,7 @@ class Box(visual.box):
  	def __init__(self, size=(0.,0.,0.)):
  		visual.box.__init__(self)
  		self.velocity=(0,0,0)
+ 		self.acceleration=(0,0,0)
  		self.masa=0
 
  	def setPos(self, vector):
