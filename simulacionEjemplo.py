94e716cecb42c201ca875aba4b6a51eba0fe937f
dario molina <darvak8@gmail.com>
2014-11-24 00:00:52 -0300

    simulacion ejemplo con 2 cuerpos geometricos

--- simulacionEjemplo.py
+++ simulacionEjemplo.py
@@ -1,50 +1,56 @@
 import visual
-# caja=visual.box(pos=visual.vector(4,2,3), size=(8.,4.,6.), color=visual.color.red)
-# bola=visual.sphere(pos=visual.vector(0,0,0), radius=3, color=visual.color.green)
 # flecha=visual.arrow(pos=visual.vector(0,0,0), axis=(-3,-5,-1), color=visual.color.white)
+# caja=visual.box(pos=visual.vector(4,2,3), size=(0.5,0.4,0.6), color=visual.color.red)
+# bola=visual.sphere(pos=visual.vector(-3,0,0), radius=0.5, color=visual.color.green)
 
 class Ball(visual.sphere):
 	def __init__(self):
-		visual.sphere.__init__(self,radius=0)
-		self.velocity=(0,0,0)
-		self.masa=0
+ 		visual.sphere.__init__(self,radius=0)
+ 		self.velocity=(0,0,0)
+ 		self.masa=0
 
-	def setPos(self, vector):
-		self.pos=vector
+ 	def setPos(self, pos):
+ 		self.pos=pos
 
-	def setRadius(self, radius):
-		self.radius=radius
+ 	def setRadius(self, radius):
+ 		self.radius=radius
 
-	def setColor(self, color):
-		self.color=color
+ 	def setColor(self, color):
+ 		self.color=color
 
-	def setVelocity(self, velocity):
-		self.velocity=velocity
+ 	def setVelocity(self, velocity):
+ 		self.velocity=velocity
 
 
 class Box(visual.box):
-	def __init__(self, size=(0.,0.,0.)):
-		visual.box.__init__(self)
-		self.velocity=(0,0,0)
-		self.masa=0
+ 	def __init__(self, size=(0.,0.,0.)):
+ 		visual.box.__init__(self)
+ 		self.velocity=(0,0,0)
+ 		self.masa=0
 
-	def setPos(self, vector):
-		self.pos=vector
+ 	def setPos(self, vector):
+ 		self.pos=vector
 
-	def setSize(self, size):
-		self.size=size
+ 	def setSize(self, size):
+ 		self.size=size
 
-	def setColor(self, color):
-		self.color=color
+ 	def setColor(self, color):
+ 		self.color=color
 
-	def setVelocity(self, velocity):
-		self.velocity=velocity
+ 	def setVelocity(self, velocity):
+ 		self.velocity=velocity
 
-ball1=Ball()
-box1=Box()
-ball1.setRadius(1)
-ball1.setPos((1,4,5))
-
-box1.setSize((8.,4.,6))
-
-		
+ball1=Ball() #create ball
+box1=Box() #create box
+box1.setSize((0.1,0.1,0.1))
+ball1.setRadius(0.05)
+r=visual.vector(-1,0,0) #initial position of the ball
+h=visual.vector(3,0,0) #initial position of the box
+z=-100
+while(z<100):
+ 	visual.rate(100)
+ 	ball1.setPos(r)
+ 	box1.setPos(h)
+ 	r.x=r.x+0.001
+ 	h.x=h.x-0.001
+	print ball1.pos.x
